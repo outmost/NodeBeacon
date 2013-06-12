eb74e40e396a868db443b9b92ab954332fcaef05
Tom Sutton <tomsutton1984@gmail.com>
2013-06-12 11:00:54 +0100

    Package.json updated

--- beacon/app.js
+++ beacon/app.js
@@ -15,7 +15,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.enable('trust proxy');
